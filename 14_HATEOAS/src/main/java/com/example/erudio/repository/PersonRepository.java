65b48b1de36a4f3207261d65d72bfa11979bf119
Alexandre Rebechi <88150414+AlexandreRebechi@users.noreply.github.com>
2026-06-01 14:31:40 -0300

    Delete 14_HATEOAS directory

--- 14_HATEOAS/src/main/java/com/example/erudio/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.erudio.repository;
-
-import com.example.erudio.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
