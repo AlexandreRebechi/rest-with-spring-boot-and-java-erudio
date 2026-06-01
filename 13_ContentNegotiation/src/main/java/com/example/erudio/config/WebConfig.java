5fd3a4d89350324d139a19ff3a307a461bacd8d6
Alexandre Rebechi <88150414+AlexandreRebechi@users.noreply.github.com>
2026-06-01 14:29:07 -0300

    Delete 13_ContentNegotiation/src/main/java/com/example/erudio directory

--- 13_ContentNegotiation/src/main/java/com/example/erudio/config/WebConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.erudio.config;
-
-public class WebConfig {
-}
--- 13_ContentNegotiation/src/main/java/com/example/erudio/serialization/converter/YamlJackson2HttpMessageConverter.java
+++
@@ -1,4 +0,0 @@
-package com.example.erudio.serialization.converter;
-
-public class YamlJackson2HttpMessageConverter {
-}
