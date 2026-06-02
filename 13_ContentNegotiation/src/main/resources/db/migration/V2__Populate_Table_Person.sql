994d70a4d914ea1fb918a91e936f44745e096760
Alexandre Rebechi <88150414+AlexandreRebechi@users.noreply.github.com>
2026-06-01 21:40:21 -0300

    Implementing Content Negotiation with JSON, XML and YAML

---
+++ 13_ContentNegotiation/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'São Paulo - brasil', 'Ayrton', 'Male', 'Senna'),
+    (2, 'Anchiano - Italy - 1500', 'Leonardo', 'Male', 'da Vinci'),
+    (4, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+    (5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+    (7, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+    (9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (11, 'Smiljan - Croatia', 'Nikolas', 'Male', 'Tesla');
+
+
