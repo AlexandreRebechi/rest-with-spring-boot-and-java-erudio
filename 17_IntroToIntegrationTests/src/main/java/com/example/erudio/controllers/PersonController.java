94ab62b767d6bda04550356f201638c5af9dd974
Alexandre Rebechi <alexandrerebechi.pf280@academico.ifsul.edu.br>
2026-06-26 15:29:54 -0300

    Introduction to Integration Testing with Testcontainers and Rest-Assured

---
+++ 17_IntroToIntegrationTests/src/main/java/com/example/erudio/controllers/PersonController.java
@@ -0,0 +1,81 @@
+package com.example.erudio.controllers;
+
+import com.example.erudio.controllers.docs.PersonControllerDocs;
+import com.example.erudio.data.dto.PersonDTO;
+import com.example.erudio.unittests.services.PersonServices;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
+public class PersonController implements PersonControllerDocs {
+
+    @Autowired
+    private PersonServices service;
+    // private PersonServices service = new PersonServices();
+
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+
+    @Override
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    @Override
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ 17_IntroToIntegrationTests/src/test/java/com/example/erudio/config/TestConfigs.java
@@ -0,0 +1,5 @@
+package com.example.erudio.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+}
