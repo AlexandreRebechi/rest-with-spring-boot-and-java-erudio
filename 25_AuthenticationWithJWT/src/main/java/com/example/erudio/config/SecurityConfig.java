c1a9d8e9b2300e8746d44585be7c55b6d0a06ce4
Alexandre Rebechi <alexandrerebechi.pf280@academico.ifsul.edu.br>
2026-08-31 10:06:37 -0300

    Implementing Authentication with Spring Security and JWT

--- 25_AuthenticationWithJWT/src/main/java/com/example/erudio/config/SecurityConfig.java
+++ 25_AuthenticationWithJWT/src/main/java/com/example/erudio/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                                 .requestMatchers(
                                "/auth/signin",
                                         "/auth/refresh/**",
-                                        "/auth/createUser", //remover depois do estudo
+                                        "/auth/createUser", // Endpoint utilizado para criação de novos usuários.
                                         "/swagger-ui/**",
                                         "/v3/api-docs/**"
                                 ).permitAll()
