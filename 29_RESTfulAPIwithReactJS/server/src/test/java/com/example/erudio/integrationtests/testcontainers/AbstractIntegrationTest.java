cb2f297a327512a4ce8911afc4690088fded49ff
Alexandre Rebechi <alexandrerebechi.pf280@academico.ifsul.edu.br>
2026-09-18 13:18:37 -0300

    Consuming our RESTful API with React JS and Kubernetes

--- 29_RESTfulAPIwithReactJS/server/src/test/java/com/example/erudio/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ 29_RESTfulAPIwithReactJS/server/src/test/java/com/example/erudio/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -20,7 +20,7 @@ public class AbstractIntegrationTest {
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-        static MySQLContainer mysql = new MySQLContainer("mysql:9.7.1")
+        static MySQLContainer mysql = new MySQLContainer("mysql:9.7.2")
                 .withDatabaseName("test")
                 .withUsername("test")
                 .withPassword("test")
